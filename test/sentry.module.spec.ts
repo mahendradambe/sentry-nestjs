ee1b05ef4c7f99af0ad482c6109b4e7660b64399
Mahendra D <mahendrad@consultbop.com>
2021-09-01 22:31:57 +0530

    test(sentry.module): add tests

--- test/sentry.module.spec.ts
+++ test/sentry.module.spec.ts
@@ -11,9 +11,69 @@ describe( "SentryModule", () => {
             it( 'should provide SentryService', async () => {
 
                 const app: TestingModule = await Test.createTestingModule( {
-
                     imports: [
                         SentryModule.forRoot( {} )
+                    ]
+                } ).compile();
+
+                const sentryService = app.get<SentryService>( SENTRY );
+
+                expect( sentryService ).toBeDefined()
+                expect( sentryService ).toBeInstanceOf( SentryService )
+
+            } );
+
+        } )
+
+    } )
+
+    describe( 'when using `forRootAsync`', () => {
+
+        describe( 'and called with `useFactory`', () => {
+
+            it( 'should provide SentryService', async () => {
+
+                const app: TestingModule = await Test.createTestingModule( {
+
+                    imports: [
+                        SentryModule.forRootAsync( {
+                            useFactory: () => ( {} )
+                        } )
+                    ]
+
+                } ).compile();
+
+                const sentryService = app.get<SentryService>( SENTRY );
+
+                console.log( sentryService )
+
+                expect( sentryService ).toBeDefined()
+                expect( sentryService ).toBeInstanceOf( SentryService )
+
+            } );
+
+        } )
+
+        describe( 'and called with `useClass`', () => {
+
+            it( 'should provide SentryService', async () => {
+
+                class OptionsFactory {
+
+                    createNestjsSentryModuleOptions() {
+
+                        return {}
+
+                    }
+
+                }
+
+                const app: TestingModule = await Test.createTestingModule( {
+
+                    imports: [
+                        SentryModule.forRootAsync( {
+                            useClass: OptionsFactory
+                        } )
                     ]
 
                 } ).compile();
